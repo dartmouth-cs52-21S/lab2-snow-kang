e1df999e8f4c0c36274c274992c0decf9ce5e864
Snow Kang <38738497+snow-kang@users.noreply.github.com>
2021-04-13 01:40:05 -0400

    Finished base version

--- main.js
+++ main.js
@@ -1,29 +1,59 @@
-$("#calculate").on("click", function (e) {
-  // gather all checked radio-button values into an array
-  var choices = $("input[type='radio']:checked")
-    .map(function (i, radio) {
-      return $(radio).val();
-    })
-    .toArray();
-  console.log(choices);
-  // now you have an array of choices = ["valueofradiobox1", "valueofradiobox2", "valueofradiobox2"]
-  // you'll need to do some calculations with this
-  // a naive approach would be to just choose the most common option - seems reasonable
+const numQuestions = 2;
 
-  $(".modal").show();
+document.getElementById("calculate").addEventListener("click", () => {
+  const choices = { touch: 0, gifts: 0, words: 0, service: 0, time: 0 };
+  let highestFrequency = 0;
+  let chosen;
+
+  // Populates a dictionary with choice: frequency pairs
+  // TODO: account for when two choices have the same frequency (randomly choose one?)
+  document
+    .querySelectorAll(".choices > label > input[type='radio']:checked")
+    .forEach((checked) => {
+      choices[checked.value] += 1;
+      if (choices[checked.value] > highestFrequency) {
+        highestFrequency = choices[checked.value];
+        chosen = checked.value;
+      }
+    });
+
+  // Displays modal with text dependent on whether all questions have been answered
+  if (
+    document.querySelectorAll(".choices > label > input[type='radio']:checked")
+      .length == 2
+  ) {
+    document.querySelector(
+      ".modal p"
+    ).innerHTML = `Your love language is ${chosen}!`;
+  } else {
+    document.querySelector(".modal p").innerHTML =
+      "Please answer all questions first!";
+  }
+
+  document.querySelector(".modal").style.display = "block";
 });
 
-// When the user clicks on (x), close the modal
-$(".close").on("click", function (e) {
-  $(".modal").hide();
-});
+document
+  .querySelectorAll(".choices > label > input[type='radio']")
+  .forEach((input) => {
+    input.addEventListener("change", (e) => {
+      e.target.parentNode.style.opacity = "1";
+      e.target.parentNode.parentNode
+        .querySelectorAll("input:not(:checked)")
+        .forEach((uncheckedInput) => {
+          uncheckedInput.parentNode.style.opacity = "0.2";
+        });
+    });
+  });
+
+// When the user clicks x, close the modal 
+document.querySelector(".close").addEventListener("click", () => {
+  document.querySelector(".modal").style.display = "none";
+})
 
 // When the user clicks anywhere outside of the modal, close it
-$(window).on("click", function (e) {
-  
-  if (e.target == $(".modal")[0]) {
-    $(".modal").hide();
-  } else {
-    console.log('test');
+window.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal")) {
+    e.target.style.display = "none";
   }
-});
+})
